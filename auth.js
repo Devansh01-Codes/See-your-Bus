0a78805ab873526f6852370363f0e54f510a5d5f
Devansh01-Codes <devansh110705@gmail.com>
2026-04-17 12:13:56 +0530

    Update auth.js

--- auth.js
+++ auth.js
@@ -6,6 +6,8 @@ import {
     signInWithEmailAndPassword,
     updateProfile 
 } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";    
+import { getDatabase, ref, set } 
+from "https://www.gstatic.com/firebasejs/10.8.0/firebase-database.js";
 
 // 2. Firebase Config
 const firebaseConfig = {
@@ -18,6 +20,7 @@ const firebaseConfig = {
     appId: "1:183488595656:web:5e0f1cf3ca1601e08bc7fd"
 };
 const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
 const auth = getAuth(app);
 
 // 3. Select Elements
